cc5f07af6720806702fa1727f8dbb04115882eaa
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2023-10-04 23:32:07 +0530

    wip

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
-class Solution(object):
-    def eraseOverlapIntervals(self, intervals):
+class StockSpanner(object):
+
+    def __init__(self):
+        self.prices = []
+        self.stack = []
+
+    def next(self, price):
         """
-        :type intervals: List[List[int]]
+        :type price: int
         :rtype: int
         """
+        prices = self.prices
+        stack = self.stack
+        prices.append(price)
+        idx = len(prices) - 1
+        while stack and prices[stack[-1]] <= price:
+            stack.pop()
+        prev_index = stack[-1] if stack else 0
+        stack.append(idx)
+        return idx - prev_index
+
+    # input2 = [[1, 2], [2, 3], [3, 4], [1, 3]]
 
 
-# node = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(5))), TreeNode(3, TreeNode(6), TreeNode(7)))
-# input = TreeNode(1, TreeNode(2), TreeNode(3))
-# input1 = TreeNode(1, TreeNode(2), TreeNode(3))
-# input = TreeNode(1, TreeNode(2), TreeNode(3))
-# input = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3, TreeNode(5), TreeNode(6)))
-# input1 = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6)))
-# input1 = TreeNode(-100, TreeNode(-200, TreeNode(-20), TreeNode(-5)), TreeNode(-300, TreeNode(-10)))
-# input1 = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7))
-# input1 = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(5))), TreeNode(3))
-# input1 = TreeNode(2)
-input2 = 'abc'
-trie = Trie()
-# trie.insert('app')
-# trie.insert('apple')
-# trie.insert('beer')
-trie.insert('add')
-# trie.insert('jam')
-# trie.insert('rental')
-# trie.insert('apps')
-# trie.insert('ap')
-trie.insert('ad')
-# output = trie.search('abc')
-# output = trie.search('apps')
-# print(output)
-# output = trie.search('app')
-# print(output)
-output = trie.search('ad')
-# output = trie.startsWith('ad')
+# input2 = [[1, 2], [1, 2], [1, 2]]
+# input2 = [[1, 100], [11, 22], [1, 11], [2, 12]]
+# input2 = [[-52, 31], [-73, -26], [82, 97], [-65, -11], [-62, -49], [95, 99], [58, 95], [-31, 49], [66, 98], [-63, 2],
+#           [30, 47], [-40, -26]]
+# input2 = [[0, 2], [1, 3], [2, 4], [3, 5], [4, 6]]
+# input2 = 'God Ding'
+s = StockSpanner()
+
+output = s.next(100)
 print(output)
+output = s.next(80)
+print(output)
+output = s.next(60)
+print(output)
+output = s.next(70)
+print(output)
+output = s.next(60)
+print(output)
+output = s.next(75)
+print(output)
+output = s.next(85)
+print(output)
+
+# output = s.next(1)
+# print(output)
+# output = s.next(2)
+# print(output)
+# output = s.next(3)
+# print(output)
+# output = s.next(4)
+# print(output)
+# output = s.next(5)
+# print(output)
+# output = s.next(6)
+# print(output)
+# output = s.next(7)
+# print(output)
