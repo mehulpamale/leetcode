81610b7e343e50dfe1912f55cd76666d99a5a74f
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2023-10-16 22:03:53 +0530

    spiral matrix

--- main.py
+++ main.py
@@ -1,22 +1,84 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
-    def reverse(self, x):
+    def spiralMatrix(self, m, n, head):
         """
-        :type x: int
-        :rtype: int
+        :type m: int
+        :type n: int
+        :type head: Optional[ListNode]
+        :rtype: List[List[int]]
         """
-        ans = 0
-        min_limit = - 2 ** 31
-        max_limit = -min_limit - 1
-        while x != 0:
-            ans *= 10
-            ans += x % 10
-            x //= 10
-            if ans < min_limit or ans > max_limit:
-                return -1
-        return ans
+        arr = []
+        for i in range(m):
+            arr.append([-1] * n)
+
+        right = ListNode(1)
+        down = ListNode(-2)
+        left = ListNode(-1)
+        up = ListNode(2)
+
+        right.next = down
+        down.next = left
+        left.next = up
+        up.next = right
+
+        direction = right
+
+        i, j = 0, 0
+        m -= 1
+        n -= 1
+        while head:
+
+            num = head.val
+            head = head.next
+
+            arr[i][j] = num
+
+            dir_val = direction.val
+            if dir_val == 1:
+                if j == n:
+                    j -= 1
+                    n -= 1
+                else:
+                    j += 1
+            elif dir_val == -2:
+                if i == m:
+                    i -= 1
+                    m -= 1
+                else:
+                    i += 1
+            elif dir_val == -1:
+                if j == 0:
+                    j -= 1
+                    n -= 1
+                else:
+                    j -= 1
+            elif dir_val == -2:
+                if i == 0:
+                    i -= 1
+                    m -= 1
+                else:
+                    i -= 1
+
+        return arr
 
 
-ip = -123
-ip2 = 1
-output = Solution().reverse(ip)
+ip = [0]
+ip2 = 3
+output = Solution().spiralMatrix(3, 4, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6,
+                                                                                                            ListNode(7,
+                                                                                                                     ListNode(
+                                                                                                                         8,
+                                                                                                                         ListNode(
+                                                                                                                             9,
+                                                                                                                             ListNode(
+                                                                                                                                 10,
+                                                                                                                                 ListNode(
+                                                                                                                                     11,
+                                                                                                                                     ListNode(
+                                                                                                                                         12)))))))))))))
 print(output)
