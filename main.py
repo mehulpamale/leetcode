5807492476d71c7b3b22fab227dfbdca5cc81d8b
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2023-10-24 11:46:51 +0530

    wip

--- main.py
+++ main.py
@@ -5,80 +5,28 @@ class ListNode:
 
 
 class Solution(object):
-    def spiralMatrix(self, m, n, head):
+    def isPalindrome(self, s):
         """
-        :type m: int
-        :type n: int
-        :type head: Optional[ListNode]
-        :rtype: List[List[int]]
+        :type s: str
+        :rtype: bool
         """
-        arr = []
-        for i in range(m):
-            arr.append([-1] * n)
-
-        right = ListNode(1)
-        down = ListNode(-2)
-        left = ListNode(-1)
-        up = ListNode(2)
-
-        right.next = down
-        down.next = left
-        left.next = up
-        up.next = right
-
-        direction = right
-
-        i, j = 0, 0
-        m -= 1
-        n -= 1
-        while head:
-
-            num = head.val
-            head = head.next
-
-            arr[i][j] = num
-
-            dir_val = direction.val
-            if dir_val == 1:
-                if j == n:
-                    j -= 1
-                    n -= 1
-                else:
-                    j += 1
-            elif dir_val == -2:
-                if i == m:
-                    i -= 1
-                    m -= 1
-                else:
-                    i += 1
-            elif dir_val == -1:
-                if j == 0:
-                    j -= 1
-                    n -= 1
-                else:
-                    j -= 1
-            elif dir_val == -2:
-                if i == 0:
-                    i -= 1
-                    m -= 1
-                else:
-                    i -= 1
-
-        return arr
+        p1, p2 = 0, len(s) - 1
+        while p1 < p2:
+            p1char = s[p1].lower()
+            p2char = s[p2].lower()
+            if not p1char.isalnum():
+                p1 += 1
+            elif not p2char.isalnum():
+                p2 -= 1
+            elif p1char.isalnum() and p2char.isalnum() and p1char == p2char:
+                p1 += 1
+                p2 -= 1
+            else:
+                return False
+        return True
 
 
-ip = [0]
+ip = ' '
 ip2 = 3
-output = Solution().spiralMatrix(3, 4, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6,
-                                                                                                            ListNode(7,
-                                                                                                                     ListNode(
-                                                                                                                         8,
-                                                                                                                         ListNode(
-                                                                                                                             9,
-                                                                                                                             ListNode(
-                                                                                                                                 10,
-                                                                                                                                 ListNode(
-                                                                                                                                     11,
-                                                                                                                                     ListNode(
-                                                                                                                                         12)))))))))))))
+output = Solution().isPalindrome(ip)
 print(output)
