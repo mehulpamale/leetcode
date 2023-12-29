15d2274afec84bf977a8e3dc324a6851a0d9e637
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2023-12-29 20:14:26 +0530

    wip

--- main.py
+++ main.py
@@ -12,15 +12,47 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def partition(self, s):
+    def intToRoman(self, num):
         """
-        :type s: str
-        :rtype: List[List[str]]
+        :type num: int
+        :rtype: str
         """
-        
+        tenxzeros = {
+            0: 'I',
+            1: 'X',
+            2: 'C',
+            3: 'M',
+        }
+        fivexzeros = {
+            0: 'V',
+            1: 'L',
+            2: 'D',
+        }
+        res = ''
+        no_of_zeros = 0
+        while num > 0:
+            divisor = 10
+            rem = num % divisor
+            num //= divisor
+            prefix = ''
+            if rem < 4:
+                prefix = tenxzeros[no_of_zeros] * rem
+            elif rem == 4:
+                prefix = tenxzeros[no_of_zeros] + fivexzeros[no_of_zeros]
+            elif rem == 5:
+                prefix = fivexzeros[no_of_zeros]
+            elif rem < 9:
+                prefix = tenxzeros[no_of_zeros] * rem
+            elif rem == 9:
+                prefix = tenxzeros[no_of_zeros] + fivexzeros[no_of_zeros]
+            res = prefix + res
+
+            no_of_zeros += 1
+
+        return res
 
 
-ip = 'aaab'
+ip = 1451
 ip2 = 3
-output = Solution().partition(ip)
+output = Solution().intToRoman(ip)
 print(output)
