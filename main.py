5b7e5425c9c28c381ac975362622c6cd34ef1cfa
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2024-01-07 23:11:19 +0530

    wip

--- main.py
+++ main.py
@@ -16,47 +16,48 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def findSubstring(self, s, words):
+
+    def nextPermutation(self, nums):
         """
-        :type s: str
-        :type words: List[str]
-        :rtype: List[int]
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        total_word_count = len(words)
-        word_length = len(words[0])
-        length = word_length * total_word_count
-        res = []
 
-        original_counter = {}
-        for i in words:
-            if i in original_counter:
-                original_counter[i] += 1
-            else:
-                original_counter[i] = 1
+        def swap(a, b):
+            nums[a], nums[b] = nums[b], nums[a]
 
-        for i in range(len(s) - length + 1):
-            curr = s[i: i + length]
-            curr_words = [''] * total_word_count
+        length = len(nums)
 
-            for index, val in enumerate(curr):
-                curr_words[(index // word_length)] += val
+        if length == 1:
+            return
 
-            curr_word_counter = {k: 0 for k in curr_words}
+        if length == 2:
+            swap(0, 1)
+            return
 
-            for index in range(total_word_count):
-                if curr_words[index] in words:
-                    curr_word_counter[curr_words[index]] += 1
+        p2 = length - 1
+        while p2 >= 0:
+            p1 = p2 - 1
 
-            for k, value in original_counter.items():
-                if k not in curr_word_counter or value != curr_word_counter[k]:
-                    break
-            else:
-                res.append(i)
+            if p2 == 0:
+                nums.sort()
+                return
 
-        return res
+            if nums[p1] < nums[p2]:
+                next_minimum = p2
+                for i in range(p2, length):
+                    if nums[p1] < nums[i] < nums[next_minimum]:
+                        next_minimum = i
+                swap(p1, next_minimum)
+                msorted = sorted(nums[p2: length])
+                for i in range(len(msorted)):
+                    nums[p2 + i] = msorted[i]
+                return
+            p2 -= 1
 
 
-# ip = 2, 10
+# ip = [2, 10]
+# ip = [2]
 # ip = [1, 3, 2, 1, 3, 5], 3
 # ip = [1, 0, -1, 0, -2, 2], 0
 # ip = "aa", "a"
@@ -68,7 +69,12 @@ class Solution(object):
 # ip = 'madhatsadbadhad', ['sad', 'bad', 'hat']
 # ip = "barfoothefoobarman", ["foo", "bar"]
 # ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "word"]
-ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "good"]
+# ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "good"]
+# [6, 5, 4, 3, 2, 1]
+ip = [1, 5, 4, 3, 2]
+# ip = [1, 4, 5, 3, 2]
+# ip = [1, 3, 5, 2, 4]
+# ip = [1, 5, 3, 4, 2]
 # ip = 'pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel', [
 #     "dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj",
 #     "lnle", "sefg", "vimw", "bxcb"]
@@ -95,8 +101,8 @@ ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "good"]
 # ip = [2], 2
 # ip = [1, 1]
 ip2 = 3
-output = Solution().findSubstring(*ip)
-print(ip[0])
+output = Solution().nextPermutation(ip)
+print(ip)
 print(output)
 
 # nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]  # Input array
