c42ab616f7b4c1c63793a10240b0aa6f422a33a7
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2024-01-12 23:32:31 +0530

    wip

--- main.py
+++ main.py
@@ -17,96 +17,46 @@ class TreeNode(object):
 
 class Solution(object):
 
-    def mySqrt(self, x):
+    def longestConsecutive(self, nums):
         """
-        :type x: int
+        :type nums: List[int]
         :rtype: int
         """
+        mmin, mmax = 10 ** 9, -10 ** 9
+        mset = set()
+        for num in nums:
+            mmin = min(num, mmin)
+            mmax = max(num, mmax)
+            mset.add(num)
 
-        if x == 0:
-            return 0
-        if x == 1 or x == 2 or x == 3:
-            return 1
+        counter = 0
+        max_len = 0
+        last = None
+        for num in range(mmin, mmax + 1):
+            if num not in mset:
+                continue
 
-        i = 2
-        while i * i * i * i <= x:
-            i *= i
+            if last is None:
+                last = num
+                counter = 1
+                continue
+            elif last + 1 != num:
+                max_len = max(max_len, counter)
+                last = num
+                counter = 1
+                continue
+            else:
+                counter += 1
 
-        upper, lower = i * i * i * i, i
+            last = num
 
-        final = None
-        while lower < upper:
-            mid = (lower + upper) // 2
-            ans = mid * mid
+        max_len = max(max_len, counter)
 
-            if ans < x:
-                if (mid + 1) * (mid + 1) > x:
-                    final = mid
-                    break
-
-                lower = mid + 1
-            elif ans > x:
-                if (mid - 1) * (mid - 1) < x:
-                    final = mid - 1
-                    break
-
-                upper = mid - 1
-            elif ans == x:
-                final = mid
-                break
-
-        if lower == upper:
-            final = lower
-
-        return final if final < 2 ** 31 - 1 else 2 ** 31 - 1
+        return max_len
 
 
-# ip = [2, 10]
-# ip = [2]
-# ip = [1, 3, 2, 1, 3, 5], 3
-# ip = [1, 0, -1, 0, -2, 2], 0
-# ip = "aa", "a"
-# ip = "aa", "a*"
-# ip = "a", "a"
-# ip = "ab", ".*"
-# ip = "aab", "c*a*b"
-# ip = "aaaaaaaaaaaaaaaaaaab", "ab"
-# ip = 'madhatsadbadhad', ['sad', 'bad', 'hat']
-# ip = "barfoothefoobarman", ["foo", "bar"]
-# ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "word"]
-# ip = "wordgoodgoodgoodbestword", ["word", "good", "best", "good"]
-# [6, 5, 4, 3, 2, 1]
-ip = 3
-# ip = [1, 4, 5, 3, 2]
-# ip = [1, 3, 5, 2, 4]
-# ip = [1, 5, 3, 4, 2]
-# ip = 'pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel', [
-#     "dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj",
-#     "lnle", "sefg", "vimw", "bxcb"]
-# ip = [2, 2, 2, 2, 2], 8
-# ip = [0, 0, 0, 0], 0
-
-# ip = [1], 1
-# ip = [2], 3
-# ip = [4, 2, 0, 2, 2, 1, 4, 4, 1, 4, 3, 2], 2
-# ip = [1, 3, 2, 1, 3, 5], 1
-# ip = [3, 2, 2, 3], 3
-# ip = [1, 2, 5, 6], 5
-# ip = [1, 3, 5], 4
-# ip = [1, 3, 5], 2
-# ip = "23"
-# ip = 2.00000, 10
-# ip = 2, 4
-# ip = [0, 1, 2]
-# ip = [10, 9, 8, 7, 6, 5, 4, 3, 2]
-# ip = [7, 8, 9, 11, 12]
-# ip = [1, 2, 0]
-# ip = [0, 0, 0, 1, 2, 3, 3, 3], 3
-# ip = [2, 2], 2
-# ip = [2], 2
-# ip = [1, 1]
-ip2 = 3
-output = Solution().mySqrt(ip)
+ip = [100, 4, 200, 1, 3, 2, 10, 11, 12, 13, 14]
+output = Solution().longestConsecutive(ip)
 print(ip)
 print(output)
 
