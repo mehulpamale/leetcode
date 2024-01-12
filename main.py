d864f32efde1f55a7bd2e627c17f4b8dbf068210
Mehul Pamale <t_mehul.pamale@tatadigital.com>
2024-01-13 02:24:45 +0530

    wip

--- main.py
+++ main.py
@@ -17,26 +17,43 @@ class TreeNode(object):
 
 class Solution(object):
 
-    def longestConsecutive(self, nums):
+    def partition(self, s):
         """
-        :type nums: List[int]
-        :rtype: int
+        :type s: str
+        :rtype: List[List[str]]
         """
-        mset = set(nums)
 
-        max_len = 0
-        for i in mset:
-            counter = 1
-            while i + counter in mset:
-                counter += 1
-            max_len = max(counter, max_len)
-        return max_len
+        def is_palindrome(a, b):
+            while a < b:
+                if s[a] != s[b]:
+                    return False
+                a += 1
+                b -= 1
+            return True
+
+        res = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+
+            for j in range(i, len(s)):
+                if is_palindrome(i, j):
+                    part.append(s[i: j + 1])
+                    dfs(j + 1)
+                    part.pop()
+
+        dfs(0)
+        return res
 
 
-ip = [100, 4, 200, 1, 3, 2, 10, 11, 12, 13, 14]
-output = Solution().longestConsecutive(ip)
-print(ip)
-print(output)
+# ip = 'a'
+ip = 'abcbacab'
+output = Solution().partition(ip)
+print('input: ', ip)
+print('output:', output)
 
 # nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]  # Input array
 # expectedNums = [0, 1, 2, 3, 4]  # The expected answer with correct length
